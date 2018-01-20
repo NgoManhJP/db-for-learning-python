89ff57b610e31f5e59fb90af98aef253dabd3f46
Ngo Manh <ngovanmanh88@gmail.com>
2018-01-20 10:51:47 +0900

    Search and download images from Flickr

--- FlickrAPI/download.py
+++ FlickrAPI/download.py
@@ -6,8 +6,8 @@ import os, time, sys, traceback    #OSの情報を得るため
 
 # Flickr APIキーの情報
 # https://www.flickr.com/services/apps/create/apply/
-flickr_api_key = ""
-secret_key = ""
+flickr_api_key = "7f4cb49ab13f787e95d7ae1555f31d--"
+secret_key = "24e71c0af644fb--"
 
 keyword = sys.argv[1] #argument value パラメータの値
 
@@ -26,16 +26,33 @@ if __name__ == '__main__':
         media = 'photos',       #　検索データの種類　→　写真を指定する
         sort = 'relevance',     #　データのソート順：検索の関連順に並べる
         safe_search = 1,        #　有害コンテンツ表示しないオプション
-        extras = 'url_q, licence' # 取得したいオプション値; url_q: 画像のアドレスが入っているデータ; ライセンス情報
+        extras = 'url_z, licence' # 取得したいオプション値; url_q: 画像のアドレスが入っているデータ; ライセンス情報
     )
     photos = result['photos']
-
+# height_c': 800,
+# height_l': '1024',
+# height_m': '500',
+# height_n': 320,
+# height_o': '1080',
+# height_q': '150',
+# height_s': '240',
+# height_sq': 75,
+# height_t': '100',
+# height_z': '640',
+# url_c: URL of medium 800, 800 on longest size image
+# url_m: URL of small, medium size image
+# url_n: URL of small, 320 on longest side size image
+# url_o: URL of original size image
+# url_q: URL of large square 150x150 size image
+# url_s: URL of small suqare 75x75 size image
+# url_sq: URL of square size image
+# url_t: URL of thumbnail, 100 on longest side size image
     try:
         if not os.path.exists('./image-data/' + keyword):
             os.mkdir('./image-data' + keyword)
 
         for photo in photos['photo']:
-            url_q = photo['url_q']
+            url_q = photo['url_z']
             filepath = './image-data' + keyword + '/' + photo['id'] + '.jpg'
             get_photos(url_q, filepath)
 
